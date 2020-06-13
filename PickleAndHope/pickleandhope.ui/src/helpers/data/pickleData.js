256723207479577bb15b651b589cc64a85877bb2
Maria <mariabrock414@gmail.com>
2020-06-13 13:56:56 -0500

    added a frontend ui to PH

---
+++ PickleAndHope/pickleandhope.ui/src/helpers/data/pickleData.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/prefer-default-export */
+import axios from 'axios';
+
+const getPickles = () => new Promise((resolve, reject) => {
+  axios.get('https://localhost:44383/api/pickles')
+    .then((result) => resolve(result.data))
+    .catch((error) => reject(error));
+});
+
+export { getPickles };
